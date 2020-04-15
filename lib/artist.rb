2e2eb50f067b7efbcf9400fd5721097e75947336
Abdul Khan <akhan17@outlook.com>
2020-04-15 23:23:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
 
